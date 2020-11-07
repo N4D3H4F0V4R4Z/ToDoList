5c226be53b238bfac5d14ef8753c5d7e22b2b512
Araz Nadzhafov <brainless11.5.1994@gmail.com>
2020-11-08 00:22:54 +0300

    First file

---
+++ ToDoList/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  ToDoList
+//
+//  Created by Наджафов Араз on 07.11.2020.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    // - UI
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        print("Приложение загрузилось!")
+        return true
+    }
+
+}
+
